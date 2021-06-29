4bfe99b46bbc24e58052a90d09eea30ade3fbe12
Manish Rawat <rawatmasi@gmail.com>
2021-06-29 12:30:31 +0530

    hamburger menu change

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 const hamburger = document.getElementById('hamburger');
 const navbarList = document.getElementById('navlist');
+const hamburgerIcon = document.getElementById('hamburger-icon');
 hamburger.addEventListener('click', function() {
     navbarList.classList.toggle('navbar__list-visible')
+    hamburgerIcon.classList.toggle('fa-times');
 });
 
 
