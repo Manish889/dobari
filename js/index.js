93fe446b95f0c1e4b7c1dcd3412e9d32d6198429
Manish Rawat <rawatmasi@gmail.com>
2021-07-18 08:48:14 +0530

    popup added

--- js/index.js
+++ js/index.js
@@ -6,4 +6,24 @@ hamburger.addEventListener('click', function() {
     hamburgerIcon.classList.toggle('fa-times');
 });
 
+// popup
 
+const modelOpenBtn = document.getElementById('model-open-btn');
+const modelCloseBtn = document.getElementById('model-close-btn');
+const modelWindow = document.getElementById('model');
+const overlay = document.getElementById('model__overlay');
+
+modelOpenBtn.addEventListener('click', () => {
+    modelWindow.classList.add('model-active');
+    overlay.classList.add('model__overlay-active')
+})
+
+modelCloseBtn.addEventListener('click', () => {
+    modelWindow.classList.remove('model-active');
+    overlay.classList.remove('model__overlay-active')
+})
+
+overlay.addEventListener('click', () => {
+    modelWindow.classList.remove('model-active');
+    overlay.classList.remove('model__overlay-active')
+})
