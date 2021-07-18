77f98bd39e758df08ac0af870da891a166454eb8
Manish Rawat <rawatmasi@gmail.com>
2021-07-18 12:56:13 +0530

    popup change

--- js/index.js
+++ js/index.js
@@ -7,23 +7,42 @@ hamburger.addEventListener('click', function() {
 });
 
 // popup
-
-const modelOpenBtn = document.getElementById('model-open-btn');
-const modelCloseBtn = document.getElementById('model-close-btn');
-const modelWindow = document.getElementById('model');
+const modelOpenBtn = document.querySelectorAll('[data-model-target]');
+const modelCloseBtn = document.querySelectorAll('[data-model__btn]');
 const overlay = document.getElementById('model__overlay');
 
-modelOpenBtn.addEventListener('click', () => {
-    modelWindow.classList.add('model-active');
-    overlay.classList.add('model__overlay-active')
-})
-
-modelCloseBtn.addEventListener('click', () => {
-    modelWindow.classList.remove('model-active');
-    overlay.classList.remove('model__overlay-active')
+modelOpenBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        const model = document.querySelector(button.dataset.modelTarget)
+        openModel(model);
+    })
 })
 
 overlay.addEventListener('click', () => {
-    modelWindow.classList.remove('model-active');
-    overlay.classList.remove('model__overlay-active')
+    const models = document.querySelectorAll('.model.model-active');
+    models.forEach(models => {
+        closeModel(models)
+    })
 })
+
+modelCloseBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        const model = button.closest('.model')
+        closeModel(model);
+    })
+})
+
+const openModel = (model) => {
+    if (model == null) return
+        model.classList.add('model-active');
+        overlay.classList.add('model__overlay-active')
+}
+
+const closeModel = (model) => {
+    if (model == null) return
+        model.classList.remove('model-active');
+        overlay.classList.remove('model__overlay-active')
+}
+
+
+
